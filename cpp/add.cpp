3c795b12a47d0ea330c9b12c7a32e0237ed61868
Jacob Collins <collinsjacob127@gmail.com>
2024-10-19 07:37:24 +0000

    Finished adder

--- cpp/add.cpp
+++ cpp/add.cpp
@@ -19,8 +19,9 @@
 #include <sstream>
 #include <string>
 #include <vector>
+// #include <cstring>
 
-/****************** VECTOR HELPERS ******************/
+/****************** HELPER FUNCS ******************/
 // Convert value to binary string
 template <typename T>
 std::string bin_str(T v, int nbits) {
@@ -32,7 +33,7 @@ std::string bin_str(T v, int nbits) {
 
 // Convert vector to str formatted list of binary strings
 template <typename T>
-std::string arrayToString(std::vector<T> arr, bool binary, int nbits) {
+std::string array_to_string(std::vector<T> arr, bool binary, int nbits) {
   std::stringstream ss;
   if (binary) {
     for (int i = 0; i < arr.size(); i++) {
@@ -60,22 +61,45 @@ long max(std::vector<long> arr) {
   }
   return max;
 }
+
+// Convert bin string to int. 1101 -> 13
+int bin_to_int(std::string& s) {
+  int result = 0;
+  int len = s.length();
+  for (int i = 0; i < len; ++i) {
+    if (s[i] == '1') { result += pow(2, len-1-i); }
+  }
+  return result;
+}
+
 /*********** CudaQ Funcs ************/
 // Add values 
 __qpu__ void add(cudaq::qvector<> &v_reg_1, 
                  cudaq::qvector<> &v_reg_2, 
-                 cudaq::qvector<> &s_reg) {
+                 cudaq::qvector<> &c_reg) {
   const int nbits_v = v_reg_1.size();
 
-  // x<cudaq::ctrl>(v_reg_1[nbits_v-1], v_reg_2[nbits_v-1], s_reg[nbits_v]);
-
+  // Carry
   for (int i = nbits_v-1; i >= 0; --i) {
-    // printf("%d\n", i); // 2 1 0 2 1 0? Why twice?
-    // x(v_reg_1[i]);
-    x<cudaq::ctrl>(v_reg_1[i], v_reg_2[i], s_reg[i+1]);
+    x<cudaq::ctrl>(v_reg_1[i], v_reg_2[i], c_reg[i]);
     x<cudaq::ctrl>(v_reg_1[i], v_reg_2[i]);
-    x<cudaq::ctrl>(s_reg[i], v_reg_2[i], s_reg[i+1]);
+    x<cudaq::ctrl>(c_reg[i+1], v_reg_2[i], c_reg[i]);
   }
+  x<cudaq::ctrl>(v_reg_1[0], v_reg_2[0]);
+  for (int i = 0; i < nbits_v-1; ++i) {
+    // Sum
+    x<cudaq::ctrl>(v_reg_1[i], v_reg_2[i]);
+    x<cudaq::ctrl>(c_reg[i+1], v_reg_2[i]);
+    if (i < nbits_v-1) {
+        // Undo Carry
+        x<cudaq::ctrl>(c_reg[i+2], v_reg_2[i+1], c_reg[i+1]);
+        x<cudaq::ctrl>(v_reg_1[i+1], v_reg_2[i+1]);
+        x<cudaq::ctrl>(v_reg_1[i+1], v_reg_2[i+1], c_reg[i+1]);
+    }
+  }
+  // Sum
+  x<cudaq::ctrl>(v_reg_1[nbits_v-1], v_reg_2[nbits_v-1]);
+  x<cudaq::ctrl>(c_reg[nbits_v], v_reg_2[nbits_v-1]);
   return;
 }
 
@@ -89,12 +113,12 @@ struct run_adder {
                           CallableKernel &&val2_setter) {
     cudaq::qvector v_reg_1(nbits_val); // Value 1 reg
     cudaq::qvector v_reg_2(nbits_val); // Value 2 reg
-    cudaq::qvector s_reg(nbits_sum);   // Sum reg
+    cudaq::qvector c_reg(nbits_sum);   // Sum reg
 
     val1_setter(v_reg_1);
     val2_setter(v_reg_2);
-    add(v_reg_1, v_reg_2, s_reg);
-    mz(v_reg_1, v_reg_2, s_reg);
+    add(v_reg_1, v_reg_2, c_reg);
+    mz(v_reg_1, v_reg_2, c_reg);
   }
 };
 
@@ -119,8 +143,9 @@ int main(int argc, char *argv[]) {
   int nbits_sum = nbits_val+1;
 
   // Helpful output
-  printf("Nbits_val: %d\n", nbits_val);
-  printf("Nbits_sum: %d\n", nbits_sum);
+  // printf("Input:\n");
+  // printf("Nbits_val: %d\n", nbits_val);
+  // printf("Nbits_sum: %d\n", nbits_sum);
   printf("Adding values: %ld + %ld (%s + %s)\n", 
           val1, val2, 
           bin_str(val1, nbits_val).c_str(), 
@@ -130,5 +155,16 @@ int main(int argc, char *argv[]) {
   int_setter val1_setter{.val = val1};
   int_setter val2_setter{.val = val2};
   auto counts = cudaq::sample(run_adder{}, nbits_val, nbits_sum, val1_setter, val2_setter);
-  printf("Found string %s\n", counts.most_probable().c_str());
+  std::string result = counts.most_probable();
+
+  // Printing Results
+  // printf("Output:\n");
+  // auto result_c = result.c_str();
+  // std::string val1_out = result.substr(0, nbits_val);
+  std::string val2_out = result.substr(nbits_val, nbits_val);
+  std::string sum_out = result.substr(2*nbits_val, 1) + val2_out;
+  // printf("Found string %s\n", result_c);
+  // printf("Val1 Out: %d (%s)\n", bin_to_int(val1_out), val1_out.c_str());
+  // printf("Val2 Out: %d (%s)\n", bin_to_int(val2_out), val2_out.c_str());
+  printf("Sum: %d (%s)\n", bin_to_int(sum_out), sum_out.c_str());
 }
